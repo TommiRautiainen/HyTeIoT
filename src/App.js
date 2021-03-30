9c1d17561eec9576cd4864f393c736416d79688c
TommiRautiainen <tommi.rautiainen2@edu.salpaus.fi>
2021-03-30 08:13:05 +0000

    2021

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 import Chart from "react-google-charts";
+import Header from './components/layout/Header';
+import Portfolio from './components/Portfolio';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
@@ -8,77 +11,96 @@ function App() {
 
   const [weather, setWeather] = useState(initWeather);
 
+  function convertUTCDateToLocalDate(date) {
+  new Date(date.getTime() + date.getTimezoneOffset()*60*1000);
+  return date;
+}
+
+  let chartHumData = [
+    ['Aika', '%'],
+    ['Please wait...', 0]
+  ];
+
+  let chartTempData = [
+      ['Aika','°C'],
+      ['Please wait...', 0]
+  ];
+
   fetch('https://oppilas-4.azurewebsites.net/api/HttpTriggerCSharp2?code=Zh0lRvpNdEWclxNoZGu4ANfJ8evb0qOABbfqOmDBMRvD7Csko1mSlQ==&deviceId=250033001847393035313138&amount=10')
     .then(response => response.json())
     .then(json => setWeather([...json]));
 
   let humtempkey = 1;
+
   const rows = () => weather.map(temphum => {
-  return <div key={humtempkey++}>
-    {temphum.Temp}
-  </div>
+
+    if(chartHumData[1][0] === 'Please wait...'){
+      chartHumData.pop();
+    }
+
+    if(chartTempData[1][0] === 'Please wait...'){
+      chartTempData.pop();
+    }
+  
+    chartHumData.push([String(convertUTCDateToLocalDate(new Date(temphum.Timestamp))).split(' ')[4], parseInt(temphum.Hum)])
+    chartTempData.push([String(convertUTCDateToLocalDate(new Date(temphum.Timestamp))).split(' ')[4], parseInt(temphum.Temp)])
+    
+    return <div key={humtempkey++}>
+      <b>Klo</b> {String(convertUTCDateToLocalDate(new Date(temphum.Timestamp))).split(' ')[4]} &nbsp;
+      <b>Ilmankosteus</b> {temphum.Hum} % &nbsp;
+      <b>Lämpötila</b> {temphum.Temp} °C
+    </div>
   })
 
   return (
-    <div className="App">
-    {rows()}
-  <div style={{ display: 'flex', maxWidth: 1900 }}>
-  <Chart
-    width={1900}
-    height={300}
-    chartType="ColumnChart"
-    loader={<div>Loading Chart</div>}
-    data={[
-      ['Aika', '%'],
-      ['13:45', 27],
-      ['13:40', 26],
-      ['13:35', 26],
-      ['13:30', 25.5],
-      ['13:25', 27],
-      ['13:20', 26.5],
-      ['13:15', 26],
-      ['13:10', 25],
-      ['13:05', 25],
-      ['13:00', 25.5],
-    ]}
-    options={{
-      title: 'Ilmankosteus',
-      chartArea: { width: '50%' },
-      hAxis: {
-        minValue: 0,
-      },
-    }}
-    legendToggle
-  />
-  </div>
-  <div style={{ display: 'flex', maxWidth: 900 }}>
-  <Chart
-    width={1900}
-    height={'300px'}
-    chartType="LineChart"
-    loader={<div>Loading Chart</div>}
-    data={[
-      ['Aika','°C'],
-      ['13:45', 27],
-      ['13:40', 26],
-      ['13:35', 26],
-      ['13:30', 25.5],
-      ['13:25', 27],
-      ['13:20', 26.5],
-      ['13:15', 26],
-      ['13:10', 25],
-      ['13:05', 25],
-      ['13:00', 25.5],
-    ]}
-    options={{
-      title: 'Lämpötila',
-      hAxis: { title: '', titleTextStyle: { color: '#333' } },
-      vAxis: { minValue: 0 },
-      chartArea: { width: '50%', height: '70%' },
-    }}
-  />
-</div>
+  
+   <Router>
+      <div className="App">
+      <Header />
+      <Switch>
+      
+         <Route path="/portfolio">
+            <Portfolio />
+        </Route>
+        <Route path="/">
+       
+        <div className="chartDiv">
+          {rows()}
+          <div>
+            <Chart
+              width={'100%'}
+              height={300}
+              chartType="ColumnChart"
+              loader={<div>Loading Chart</div>}
+              data={chartHumData}
+              options={{
+                title: 'Ilmankosteus',
+                chartArea: { width: '50%' },
+                vAxis: { minValue: 0 }, 
+              }}
+            />
+          </div>
+          <div>
+            <Chart
+              width={'100%'}
+              height={300}
+              chartType="LineChart"
+              loader={<div>Loading Chart</div>}
+              data={chartTempData}
+              options={{
+                title: 'Lämpötila',
+                chartArea: { width: '50%' },
+                vAxis: { minValue: 0 },
+              }}
+            />
+             <a href="https://github.com/TommiRautiainen/HyTeIoT/blob/master/README.md" target="_BLANK" rel="noopener noreferrer">Projektin README</a>
+          </div>
+          
+        </div>
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div>
+    Portfolion tekstiä
+    </div>
+  )
+}
+
+export default Portfolio;
